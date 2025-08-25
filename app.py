5ef38a52f6d3b11077d26af8c0cc0611cc525773
Marshall31 <animeo@hotmail.fr>
2025-08-25 00:27:58 +0000

    nouvelle données

--- app.py
+++ app.py
@@ -3,8 +3,18 @@ import pandas as pd
 
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
+qte = données['qte']
+prix = données['prix']
 
-figure.write_html('ventes-par-region.html')
+figure = px.pie(données, values=qte, names='produit', title='ventes par produit')
+figure2 = px.pie(données, values=qte*prix, names='produit', title='chiffre d\'affaires par produit')
+#figure2 = px.pie(données, values='prix', names='produit', title='chiffre d\'affaires par produit')
 
-print('ventes-par-région.html généré avec succès !')
+
+figure.write_html('ventes-par-roduit.html')
+figure2.write_html('chiffre-affaires-par-roduit.html')
+
+print('ventes-et-chiffre-affaires-par-roduit.html généré avec succès !')
+
+#a. les ventes par produit,
+#b. le chiffre d'affaires par produit.
